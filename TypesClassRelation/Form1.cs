c1d7b6edef0be0180b2fdde510a722c24282ad68
EvadS <Skiba.Evgeniy@gmail.com>
2014-12-07 17:10:47 +0200

    Create Repo

---
+++ TypesClassRelation/Form1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TypesClassRelation
+{
+    public partial class Form1 : Form
+    {
+        // произвольный комментарий
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}

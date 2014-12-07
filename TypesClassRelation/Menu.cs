0ae7ca4fac32825851048e9f78a4444268b45b85
EvadS <Skiba.Evgeniy@gmail.com>
2014-12-07 19:32:08 +0200

    vesion with all class but i don't write any test method

---
+++ TypesClassRelation/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * Зависимость  как тип отношений между объектами
+ */
+namespace TypesClassRelation
+{
+    public class Menu
+    {
+        private static  int i;
+        public static void ShowEmployees(TypesClassRelation.Employee[] employee)
+        {
+            System.Console.WriteLine("Список сотрудников");
+
+            for (i = 0; i < employee.Length; i++)
+            {
+                System.Console.WriteLine(employee[i].Name + " - " +
+                    employee[i].getPastPosition()[0]);
+            }
+
+
+        }
+    }
+}
---
+++ TypesClassRelation/PastPosition.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+/*
+ * пример Композиции
+ */
+namespace TypesClassRelation
+{  
+    /// <summary>
+    /// Прежняя занимаемая должность на предприятии
+    /// </summary>
+    public class PastPosition
+    {
+        private int name;
+
+        public int Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private Department departmentItem;
+
+        public Department DepartmentItem
+        {
+            get { return departmentItem; }
+            set { departmentItem = value; }
+        }
+
+        public PastPosition(string position, Department dep)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
